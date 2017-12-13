3ec0e8f5ab0c35f9a9111b38158b6d1b78e08abb
github username <github email address>
2017-12-13 16:49:52 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,18 @@
 // Code CoordinatesButton Component Here
+
+import React from 'react';
+
+class CoordinatesButton extends React.Component {
+
+  clicker = (event) => {
+    const coordinates = [event.screenX, event.screenY]
+    this.props.onReceiveCoordinates(coordinates)
+  }
+  render(){
+    return(
+      <button onClick={this.clicker}>Button</button>
+    )
+  }
+}
+
+export default CoordinatesButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,19 @@
 // Code DelayedButton Component Here
+import React from 'react';
+
+class DelayedButton extends React.Component {
+  clicker = (event) => {
+    event.persist();
+
+  setTimeout(() => {
+    this.props.onDelayedClick(event)
+  }, this.props.delay)
+}
+  render() {
+    return(
+      <button onClick={this.clicker}>Delay</button>
+    );
+  }
+}
+
+export default DelayedButton
